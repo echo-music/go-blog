5481281624b84a53c0a1a894b004e8e0431db5ba
echo music <php_fangting@126.com>
2023-04-04 07:44:33 +0800

    加入链路追踪

--- internal/service/order.go
+++ internal/service/order.go
@@ -6,11 +6,9 @@ import (
 	"github.com/echo-music/go-blog/pkg/gerror"
 	"github.com/echo-music/go-blog/pkg/model"
 	"github.com/gin-gonic/gin"
-	"github.com/uptrace/opentelemetry-go-extra/otelzap"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	oteltrace "go.opentelemetry.io/otel/trace"
-	"go.uber.org/zap"
 )
 
 type orderSrv struct {
@@ -22,46 +20,46 @@ func (a *orderSrv) List(c *gin.Context, arg model.OrderListArg) (orders model.Or
 
 	err = db.DB().Model(&model.Order{}).Scan(&orders.List).Error
 	tr := otel.Tracer("order-list")
-	_, span := tr.Start(c.Request.Context(), "list", oteltrace.WithAttributes(attribute.String("id", "100")))
 
+	_, span := tr.Start(c.Request.Context(), "list", oteltrace.WithAttributes(attribute.String("id", "100")))
 	span.SetAttributes(attribute.Int("age", 10))
-	span.SetAttributes(attribute.String("name", "张三"))
 
 	defer span.End()
 
-	otelzap.Ctx(c.Request.Context()).Error("order-list", zap.String("name", "你这次不上百哦"))
-	otelzap.L().Info("replaced zap's global loggers")
-
-	otelzap.L().Info("replaced zap's global loggers")
-
-	otelzap.L().Info("replaced zap's global loggers")
-
-	otelzap.L().InfoContext(c.Request.Context(), "为啥info不打印")
-
 	a.Create(c)
+	a.Update(c)
 	return
 }
 
 func (a *orderSrv) Create(c *gin.Context) {
-	err := gerror.New(errors.New("订单号不能为空"))
+	//err := gerror.New(errors.New("订单号不能为空"))
 
 	tr := otel.Tracer("order-Create")
 	_, span := tr.Start(c.Request.Context(), "Create", oteltrace.WithAttributes(attribute.String("id", "100")))
-
-	span.SetAttributes(attribute.Int("age", 10))
-	span.SetAttributes(attribute.String("name", "kkk"))
-	span.RecordError(err)
 	defer span.End()
 
-	if err != nil {
-		return
+	if span.IsRecording() {
+		span.SetAttributes(attribute.Int("age", 10))
+		span.SetAttributes(attribute.String("name", "kkk"))
 	}
+
+	//span.RecordError(err)
+
+	//if err != nil {
+	//	return
+	//}
 
 }
 
 func (a *orderSrv) Update(c *gin.Context) {
 	err := gerror.New(errors.New("订单号不能为空"))
+	tr := otel.Tracer("order-Update")
+	_, span := tr.Start(c.Request.Context(), "Update", oteltrace.WithAttributes(attribute.String("id", "100")))
 
+	span.SetAttributes(attribute.Int("age", 10))
+	span.SetAttributes(attribute.String("name", "kkk"))
+	//span.RecordError(err)
+	defer span.End()
 	if err != nil {
 		return
 	}
--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -2,11 +2,14 @@ package middleware
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/echo-music/go-blog/pkg/response"
 	"github.com/gin-gonic/gin"
 	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"time"
 )
 
@@ -19,8 +22,8 @@ func Logger(logger *otelzap.Logger) gin.HandlerFunc {
 		bodyLogWriter := &response.BodyLogWriter{Body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
 		c.Writer = bodyLogWriter
 		c.Next()
-
-		logger.Ctx(c.Request.Context()).Info(path,
+		span := trace.SpanFromContext(c.Request.Context())
+		logContent := []zapcore.Field{
 			zap.Int("status", c.Writer.Status()),
 			zap.String("method", c.Request.Method),
 			zap.String("path", path),
@@ -30,8 +33,33 @@ func Logger(logger *otelzap.Logger) gin.HandlerFunc {
 			zap.String("user-agent", c.Request.UserAgent()),
 			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
 			zap.Duration("cost", time.Since(start)),
-			zap.String("trace_id", trace.SpanFromContext(c.Request.Context()).SpanContext().TraceID().String()),
-			zap.String("span_id", trace.SpanFromContext(c.Request.Context()).SpanContext().SpanID().String()),
-		)
+			zap.String("trace_id", span.SpanContext().TraceID().String()),
+			zap.String("span_id", span.SpanContext().SpanID().String()),
+		}
+
+		attr := []attribute.KeyValue{
+			attribute.Int("status", c.Writer.Status()),
+			attribute.String("method", c.Request.Method),
+			attribute.String("path", path),
+			attribute.String("query", query),
+			attribute.String("res", bodyLogWriter.Body.String()),
+			attribute.String("ip", c.ClientIP()),
+			attribute.String("user-agent", c.Request.UserAgent()),
+			attribute.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			attribute.String("cost", time.Since(start).String()),
+			attribute.String("trace_id", span.SpanContext().TraceID().String()),
+			attribute.String("span_id", span.SpanContext().SpanID().String()),
+		}
+
+		span.SetAttributes(attr...)
+		defer span.End()
+
+		if c.Writer.Status() != 200 || c.Errors != nil {
+			fmt.Println(c.Writer.Status())
+			logger.Ctx(c.Request.Context()).Error(path, logContent...)
+		} else {
+			logger.Ctx(c.Request.Context()).Info(path, logContent...)
+		}
+
 	}
 }
