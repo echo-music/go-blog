3b9ff4e691fbcea53329bfdda1be90e05ce33c6f
echo music <php_fangting@126.com>
2023-04-04 17:54:21 +0800

    加入链路追踪

--- internal/service/order.go
+++ internal/service/order.go
@@ -32,7 +32,7 @@ func (a *orderSrv) List(c *gin.Context, arg model.OrderListArg) (orders model.Or
 }
 
 func (a *orderSrv) Create(c *gin.Context) {
-	//err := gerror.New(errors.New("订单号不能为空"))
+	err := gerror.New(errors.New("订单号不能为空"))
 
 	tr := otel.Tracer("order-Create")
 	_, span := tr.Start(c.Request.Context(), "Create", oteltrace.WithAttributes(attribute.String("id", "100")))
@@ -43,11 +43,11 @@ func (a *orderSrv) Create(c *gin.Context) {
 		span.SetAttributes(attribute.String("name", "kkk"))
 	}
 
-	//span.RecordError(err)
+	span.RecordError(err)
 
-	//if err != nil {
-	//	return
-	//}
+	if err != nil {
+		return
+	}
 
 }
 
