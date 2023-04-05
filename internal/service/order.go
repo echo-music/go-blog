556e6862a449faf9a53bf7a510996ac0d56917c7
echo music <php_fangting@126.com>
2023-04-05 17:08:15 +0800

    blog

--- internal/service/order.go
+++ internal/service/order.go
@@ -2,10 +2,12 @@ package service
 
 import (
 	"errors"
+	"fmt"
 	"github.com/echo-music/go-blog/pkg/db"
 	"github.com/echo-music/go-blog/pkg/gerror"
 	"github.com/echo-music/go-blog/pkg/model"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 type orderSrv struct {
@@ -14,6 +16,9 @@ type orderSrv struct {
 var Order orderSrv
 
 func (a *orderSrv) List(c *gin.Context, arg model.OrderListArg) (orders model.OrderListRet, err error) {
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("处理完成")
 	err = db.DB().Model(&model.Order{}).Scan(&orders.List).Error
 	return
 }
