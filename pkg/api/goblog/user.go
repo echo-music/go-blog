6e56f15a3952b4dfde7e057f0df48ee23e610a9e
echo music <php_fangting@126.com>
2023-04-08 14:25:35 +0800

    加入requestid

---
+++ pkg/api/goblog/user.go
@@ -0,0 +1,21 @@
+package goblog
+
+type UserCreateArg struct {
+}
+
+type UserCreateRet struct {
+}
+
+type UserListArg struct {
+	Name string `json:"name" form:"name"`
+}
+
+type UserListRet struct {
+	List []UserListRow
+}
+
+type UserListRow struct {
+	Id   uint   `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
