8dbbede475ad061e2e537c2ef36cc05b56bcf21b
echo music <php_fangting@126.com>
2023-04-08 14:02:30 +0800

    加入requestid

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -5,32 +5,28 @@ import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"sync"
-	"time"
 )
 
-var cache *redis.Client
+var cli *redis.Client
 var once sync.Once
 
 func Redis() *redis.Client {
-	return cache
+	if cli == nil {
+		panic("未初始化redis客户端")
+	}
+	return cli
 }
 
 func Init(cfg Config) {
-
 	once.Do(func() {
-		cache = redis.NewClient(&redis.Options{
+		cli = redis.NewClient(&redis.Options{
 			Addr:     cfg.Host,
 			Password: cfg.Password, // no password set
 			DB:       cfg.DB,       // use default DB
 		})
-		_, err := cache.Ping(context.Background()).Result()
+		_, err := cli.Ping(context.Background()).Result()
 		if err != nil {
 			panic(fmt.Sprintf("Redis connect ping failed, err:%+v", err))
 		}
 	})
-}
-
-func Set(key string, value interface{}, expire time.Duration) (err error) {
-	_, err = cache.Set(context.Background(), key, value, expire).Result()
-	return
 }
