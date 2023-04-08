8be2a102c80979b623a62f694f23dff03a3dfe7b
echo music <php_fangting@126.com>
2023-04-08 14:57:31 +0800

    加入requestid

--- pkg/g/g.go
+++ pkg/g/g.go
@@ -3,7 +3,10 @@ package g
 import (
 	"github.com/echo-music/go-blog/pkg/cache"
 	"github.com/echo-music/go-blog/pkg/db"
+	"github.com/echo-music/go-blog/pkg/logs"
+	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
@@ -13,4 +16,8 @@ func Redis() *redis.Client {
 
 func DB() *gorm.DB {
 	return db.DB()
+}
+
+func Zap(c *gin.Context) *zap.Logger {
+	return logs.Ctx(c)
 }
