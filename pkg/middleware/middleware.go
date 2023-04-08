927e8f78e405af4797c479d8a30cf03ae496f5ac
echo music <php_fangting@126.com>
2023-04-08 16:44:58 +0800

    加入requestid

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -8,8 +8,8 @@ import (
 func Init(r *gin.Engine) {
 	mws := []gin.HandlerFunc{
 		RequestID(),    //客户端请求ID
-		Recovery(),     //系统异常
 		Logger(),       //请求日志
+		Recovery(),     //系统异常
 		cors.Default(), //跨域
 		Catch(),        //错误输出
 	}
