ead7905f4a87c93ff15dae12e73ec7dc0269cf1b
echo music <php_fangting@126.com>
2023-04-09 10:23:36 +0800

    加入requestid

--- pkg/middleware/requestid.go
+++ pkg/middleware/requestid.go
@@ -10,7 +10,6 @@ func RequestID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 检查请求头中是否有 `X-Request-ID`，如果有则复用，没有则新建
 		requestID := c.Request.Header.Get(known.XRequestIDKey)
-
 		if requestID == "" {
 			requestID = uuid.New().String()
 		}
