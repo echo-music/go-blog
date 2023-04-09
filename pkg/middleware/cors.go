f518c9cc770c5f52f3809802b1bd4988029d34d5
echo music <php_fangting@126.com>
2023-04-09 11:29:29 +0800

    加入requestid

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -12,7 +12,7 @@ func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowAllOrigins:  true,
 		AllowOrigins:     []string{"http://localhost"},
-		AllowMethods:     []string{http.MethodConnect, http.MethodTrace, http.MethodGet, http.MethodDelete, http.MethodHead, http.MethodOptions, http.MethodPut, http.MethodPost, http.MethodPatch},
+		AllowMethods:     []string{http.MethodGet, http.MethodDelete, http.MethodHead, http.MethodOptions, http.MethodPut, http.MethodPost, http.MethodPatch, http.MethodConnect, http.MethodTrace},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Cookie", "Authorization", known.XRequestIDKey, "X-Auth-Token"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
