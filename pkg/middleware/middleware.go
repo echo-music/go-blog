343d338ba01e743e5217127d1b96db890d15f812
echo music <php_fangting@126.com>
2023-04-10 20:54:40 +0800

    加入requestid

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -10,8 +10,8 @@ func Init(r *gin.Engine) {
 		Recovery(),  //系统异常
 		Logger(),    //请求日志
 		Cors(),      //跨域
-		Csrf(),
-		Catch(), //错误输出
+		Csrf(),      //csrf攻击
+		Catch(),     //错误输出
 	}
 	r.Use(mws...)
 }
