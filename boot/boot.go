930581c7d9724a002783c3a0663f278db1276e10
echo music <php_fangting@126.com>
2023-04-18 16:54:45 +0800

    singfly

--- boot/boot.go
+++ boot/boot.go
@@ -32,7 +32,7 @@ var Cfg config
 func Run() {
 
 	//设置debug模式
-	gin.SetMode(gin.DebugMode)
+	gin.SetMode(gin.ReleaseMode)
 
 	//读取配置文件
 	if _, err := toml.DecodeFile("./config/app.toml", &Cfg); err != nil {
--- internal/service/user.go
+++ internal/service/user.go
@@ -2,22 +2,39 @@ package service
 
 import (
 	"errors"
+	"fmt"
 	"github.com/echo-music/go-blog/internal/model"
 	"github.com/echo-music/go-blog/pkg/api/goblog"
 	"github.com/echo-music/go-blog/pkg/g"
 	"github.com/echo-music/go-blog/pkg/gerror"
-	"github.com/echo-music/go-learn/events"
 	"github.com/gin-gonic/gin"
+	"github.com/golang/groupcache/singleflight"
 )
 
 type userSrv struct {
 }
 
+var single singleflight.Group
 var User userSrv
+var res goblog.UserListRet
 
 func (a *userSrv) List(c *gin.Context, arg goblog.UserListArg) (users goblog.UserListRet, err error) {
-	events.Handler(nil)
-	err = g.DB().Model(&model.User{}).Scan(&users.List).Error
+	if len(res.List) > 0 {
+		users = res
+		return users, nil
+	}
+
+	_, err = single.Do("cache", func() (interface{}, error) {
+		fmt.Println("查询db")
+		err = g.DB().Model(&model.User{}).Scan(&res.List).Error
+		return nil, err
+	})
+	if err != nil {
+		return
+	}
+
+	users = res
+
 	return
 }
 
