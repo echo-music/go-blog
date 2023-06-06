80de3c23d063c480d4413432563f5018d42bf834
echo music <php_fangting@126.com>
2023-06-06 14:21:53 +0800

    go blog

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,15 +1,9 @@
 package service
 
 import (
-	"fmt"
-	"github.com/echo-music/go-blog/internal/model"
 	"github.com/echo-music/go-blog/pkg/api/blog"
-	"github.com/echo-music/go-blog/pkg/g"
-	"github.com/echo-music/go-blog/pkg/gerror"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/groupcache/singleflight"
-	"github.com/pkg/errors"
-	"os"
 )
 
 type userSrv struct {
@@ -24,20 +18,20 @@ func (a *userSrv) List(c *gin.Context, arg blog.UserListArg) (users blog.UserLis
 		users = res
 		return users, nil
 	}
-	if _, err = os.Open("a.txt"); err != nil {
-		return res, gerror.New(errors.WithStack(err))
-	}
-
-	_, err = single.Do("cache", func() (interface{}, error) {
-		fmt.Println("查询db")
-		err = g.DB().Model(&model.User{}).Scan(&res.List).Error
-		return nil, err
-	})
-	if err != nil {
-		return
-	}
-
-	users = res
+	//if _, err = os.Open("a.txt"); err != nil {
+	//	return res, gerror.New(errors.WithStack(err))
+	//}
+	//
+	//_, err = single.Do("cache", func() (interface{}, error) {
+	//	fmt.Println("查询db")
+	//	err = g.DB().Model(&model.User{}).Scan(&res.List).Error
+	//	return nil, err
+	//})
+	//if err != nil {
+	//	return
+	//}
+	//
+	//users = res
 
 	return
 }
